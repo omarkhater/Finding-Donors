1c9407eb975849e0211cf4fc9ec5f7c3ee95415d
omarkhater <omer_ayman2009@yahoo.com>
2020-12-15 17:49:42 +0200

    Update pipeline definition with sample size modification

---
+++ Main.m
@@ -0,0 +1,17 @@
+clear;clc;close all
+submit = 1;
+%% Load the data
+census = ImportFindDonor("D:\ML\courses\MachineLearningWithTF\codes\projects\p1_charityml\census.csv", [2, Inf]);
+census.income =[];
+testcensus = ImportFindDonorTest("D:\ML\courses\MachineLearningWithTF\codes\projects\p1_charityml\test_census.csv", [2, Inf]);
+testcensus.Properties.VariableNames{1} = ' id'; 
+%% Gaussian Naive Byes classifier
+[trainedGNB, validationAccuracyGNB] = trainGNB(census);
+ytestGNB = trainedGNB.predictFcn(testcensus);
+%% Write to file to submit
+
+Tsubmission = table(testcensus.id, ytestGNB, ...
+        'VariableNames', {'id', 'income'});
+ if submit
+    writetable(Tsubmission, 'Latestsubmission_M.csv');
+ end
